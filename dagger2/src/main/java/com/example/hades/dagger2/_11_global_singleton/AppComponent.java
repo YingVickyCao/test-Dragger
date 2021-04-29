15d7c2c7e962b002c7fe5e136952dbbdf7478013
YingVickyCao <ying.vicky.cao@outlook.com>
2021-04-29 15:51:41 +0800

    init dagger2 test for android

--- dagger2/src/main/java/com/example/hades/dagger2/_11_global_singleton/AppComponent.java
+++ dagger2/src/main/java/com/example/hades/dagger2/_11_global_singleton/AppComponent.java
@@ -1,7 +1,5 @@
 package com.example.hades.dagger2._11_global_singleton;
 
-import com.example.hades.dagger2._11_global_singleton.App;
-import com.example.hades.dagger2._11_global_singleton.MainActivity;
 import com.example.hades.dagger2._11_global_singleton.modules.AppModule;
 import com.example.hades.dagger2._11_global_singleton.modules.ReceiversModule;
 import com.example.hades.dagger2._11_global_singleton.modules.UtilsModules;
@@ -15,9 +13,4 @@ public interface AppComponent {
     void inject(App app);
 
     void inject(MainActivity mainActivity);
-
-    // ERROR:Exception in thread "main" java.lang.NullPointerException
-    //	at com.example.hades.dagger2._11_global_singleton.App.create(App.java:27)
-    //	at com.example.hades.dagger2._11_global_singleton.Test.main(Test.java:6)
-//    void inject(Object object);
 }
